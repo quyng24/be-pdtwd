945babfc0b80ce468d2704124772ea03753a6c84
Nguyen Van Quy <nquy50771@gamil.com>
2025-12-10 20:57:02 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="My First Backend", version="1.0")
+app = FastAPI(title="PandaTaekwondo", version="1.0")
 
 @app.get("/")
 def home():
     return {"message": "Hello World! Backend Python của bạn đang chạy!"}
 
 @app.get("/api/user")
-def health_check():
+def get_user():
     return {"user": "Quy Nguyen"}
